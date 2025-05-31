2e3b1257cc3ad23c45de9ccd0e62ecca3fab689b
Sakshi Kumari <sakshik27yt@gmail.com>
2025-05-31 23:33:31 +0530

    ...

---
+++ qiuizAPI-Gateway/src/main/java/com/quiz/qiuizAPI_Gateway/QiuizApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.quiz.qiuizAPI_Gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QiuizApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QiuizApiGatewayApplication.class, args);
+	}
+
+}
---
+++ qiuizAPI-Gateway/src/test/java/com/quiz/qiuizAPI_Gateway/QiuizApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quiz.qiuizAPI_Gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QiuizApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
